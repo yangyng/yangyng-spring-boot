decb52b5a15bed655e55d21b6dd7907a8de5e78c
yangy <java_yangy@126.com>
2018-10-08 13:41:02 +0800

    分布式配置文件中心项目初始化

---
+++ yangyng-config/src/test/java/com/yangy/config/YangyngConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yangy.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YangyngConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
