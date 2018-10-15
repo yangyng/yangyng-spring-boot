a18785c70ab5067afdfa2fabfcebb4467938bef7
yangy <java_yangy@126.com>
2018-10-15 17:53:12 +0800

    项目初始化 修改MAVEN POM

---
+++ yangyng-gateway/src/main/java/com/yangy/gateway/filter/AccessFilter.java
@@ -0,0 +1,44 @@
+package com.yangy.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 验证过滤器
+ *
+ * @author yang yang
+ * @create 2018/10/12
+ */
+public class AccessFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        String token = request.getHeader("token");
+        if (StringUtils.isEmpty(token)) {
+//            throw new MyException
+        }
+        return null;
+    }
+}
